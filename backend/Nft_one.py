3319a2a27c2120ecda983d49e50c412484c2368b
kingpin-ay <ayushmondal001@gmail.com>
2023-07-09 21:53:12 +0530

    the base variant of a small nft contract

--- backend/Nft_one.py
+++ backend/Nft_one.py
@@ -9,18 +9,3 @@ class Nft_one(sp.Contract):
         sp.verify(self.data.owner == sp.sender , "Not the owner")
         self.data.owner =  newAddress
 
-
-alice = sp.test_account('Alice').address
-bob = sp.test_account('Bob').address
-
-@sp.add_test(name="Nft_one")
-def test():
-    ts1 = sp.test_scenario()
-    metadeta = sp.utils.metadata_of_url('https://ipfs.io/ipfs/QmRyZpcYUt2mhxYCiwvHWn5J3EYcWLC5jUaqid6AECwMmc?filename=1316806.jpeg')
-    c1 = Nft_one(address = alice, metadata = metadeta)
-    ts1 += c1
-    c1.transfer(bob).run(alice)
-    # sp.verify(c1.data.owner == bob , "Not bob")
-    
-    
-    
