117daeebc8ef463eb183517fd20f2f36835ca0b7
jishnu2021 <jishnughosh2023@gmail.com>
2023-07-09 22:52:17 +0530

    Add several webpages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Footer from './Components/Footer'
 
-import Footer from "./Components/Footer";
 
 import { Outlet } from "react-router-dom";
+
+
 
 const App = () => {
   return (
     <>
       <Navbar />
+      
       <Outlet />
+      <Footer/>
     </>
   );
 };
