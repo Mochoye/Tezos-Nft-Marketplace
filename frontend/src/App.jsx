3732d0eef386e7269e2d3e5b2f1276c239b07c19
Mochoye <archisman.gogol@gmail.com>
2023-07-09 14:13:34 +0530

    NFT card added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Card from './Components/Card'
 
 const App = () => {
   return (
     <>
-     <Navbar/> 
+     <Navbar/>
+     <div className="d-flex justify-content-around flex-wrap">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+      </div> 
     </>
   )
 }
